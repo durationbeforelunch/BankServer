22befd01c54f10e807618b5a07462ece7e993f9f
durationbeforelunch <wapshavkatkz@gmail.com>
2022-02-16 20:24:16 +0300

    Added Account Repository

---
+++ src/main/java/com/bank/server/repository/AccountRepository.java
@@ -0,0 +1,16 @@
+package com.bank.server.repository;
+
+import com.bank.server.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+    @NonNull Optional<Account> findById(@NonNull Integer id);
+
+    @NonNull List<Account> findAll();
+
+}
