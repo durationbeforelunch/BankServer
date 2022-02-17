5f8c5350cfe87dea8e4dc95fcb491a91311c8793
durationbeforelunch <wapshavkatkz@gmail.com>
2022-02-17 22:30:04 +0300

    Created Logging Aspect

---
+++ src/main/java/com/bank/server/aop/LoggingAspect.java
@@ -0,0 +1,29 @@
+package com.bank.server.aop;
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Aspect @Component
+public class LoggingAspect {
+
+    @After("execution(* com.bank.server.controller.BankController.get* (..))")
+    public void controllerGetAccountInfoLogger() {
+        log.info("Retrieving Account Information");
+    }
+    @After("execution(* com.bank.server.controller.BankController.createAccount (..))")
+    public void controllerCreateNewAccountLogger() {
+        log.info("Creating New Account");
+    }
+    @After("execution(* com.bank.server.controller.BankController.deleteAccount (..))")
+    public void controllerDeleteAccountLogger() {
+        log.info("Deleting Account");
+    }
+    @After("execution(* com.bank.server.controller.BankController.patchAccountInfo(..))")
+    public void controllerPatchAccountLogger() {
+        log.info("Patching Account info");
+    }
+
+}
