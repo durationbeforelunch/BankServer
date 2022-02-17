02bcecc10e0e6bdcdbf562452faa00d357052646
durationbeforelunch <wapshavkatkz@gmail.com>
2022-02-17 22:26:04 +0300

    Enabled AspectJ AutoProxy

--- src/main/java/com/bank/server/Starter.java
+++ src/main/java/com/bank/server/Starter.java
@@ -2,10 +2,12 @@ package com.bank.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 public class Starter {
 
     public static void main(String[] args) {
