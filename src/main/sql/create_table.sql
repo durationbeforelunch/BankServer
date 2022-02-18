a5434db60b73f98d440b1e2e39680e22cfb54232
durationbeforelunch <wapshavkatkz@gmail.com>
2022-02-18 17:32:28 +0300

    small update for Spring Security

--- src/main/sql/create_table.sql
+++ src/main/sql/create_table.sql
@@ -6,19 +6,23 @@ CREATE TABLE IF NOT EXISTS accounts
     username   varchar(50) UNIQUE NOT NULL,
     password   varchar(128)       NOT NULL,
     email      varchar(128)       NOT NULL,
-    balance    decimal   default 0 CHECK ( balance >= 0 ),
-    created_on timestamp default current_timestamp,
-    last_login timestamp default current_timestamp
+    balance    decimal     default 0 CHECK ( balance >= 0 ),
+    role       varchar(20) default 'USER',
+    status     varchar(20) default 'ACTIVE',
+    created_on timestamp   default current_timestamp,
+    last_login timestamp   default current_timestamp
 );
 
 INSERT INTO accounts (username,
                       password,
                       email,
-                      balance)
+                      balance,
+                      role)
 VALUES ('admin',
-        'admin',
-        'admin@email.com',
-        1000);
+        '$2a$12$HLDUE6cDSuMTqaj8nzsrVeWl8yK85pgIElejPeI0g30tB1K.///Sm',
+        'admin@mail.com',
+        500000,
+        'ADMIN');
 
 SELECT *
 FROM accounts;
