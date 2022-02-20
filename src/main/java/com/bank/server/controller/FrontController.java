25c6912c8c21d8b1b9230adb56d27a20c383b418
durationbeforelunch <wapshavkatkz@gmail.com>
2022-02-20 22:15:39 +0300

    moved registration endpoints from rest controller to view controller

--- src/main/java/com/bank/server/controller/FrontController.java
+++ src/main/java/com/bank/server/controller/FrontController.java
@@ -1,19 +1,43 @@
 package com.bank.server.controller;
 
+import com.bank.server.dto.AccountCreateDto;
+import com.bank.server.service.IAccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
 public class FrontController {
 
-    @GetMapping("/login")
-    public String viewLoginPage() {
-        return "login";
-    }
+    private final IAccountService accountService;
+    public static final String MODEL_NAME = "account";
 
     @GetMapping
     public String viewIndexPage() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView viewLoginPage(@ModelAttribute(MODEL_NAME) AccountCreateDto dto) {
+        return new ModelAndView("/login", MODEL_NAME, dto);
+    }
+
+    @GetMapping("/signup")
+    public String viewTest(Model model) {
+        AccountCreateDto dto = new AccountCreateDto();
+        model.addAttribute(MODEL_NAME, dto);
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public ModelAndView registerUserAccount(@ModelAttribute(MODEL_NAME) AccountCreateDto dto) {
+        accountService.createAccount(dto);
+        return new ModelAndView("/login", MODEL_NAME, dto);
     }
 
 }
