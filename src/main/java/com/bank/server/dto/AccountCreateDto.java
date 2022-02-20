9141cd44b00cd74ed5d6df980f40040c8b67c425
durationbeforelunch <wapshavkatkz@gmail.com>
2022-02-20 20:23:32 +0300

    Added lombok noArgsConstructor & Setter & removed final constraint

--- src/main/java/com/bank/server/dto/AccountCreateDto.java
+++ src/main/java/com/bank/server/dto/AccountCreateDto.java
@@ -2,15 +2,19 @@ package com.bank.server.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class AccountCreateDto implements Serializable {
 
-    private final String username;
-    private final String password;
-    private final String email;
+    private String username;
+    private String password;
+    private String email;
 
 }
